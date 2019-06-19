a4ff9243a19d413ce34942892e9e5eb039c83e20
Todd Pataky <0todd0@gmail.com>
2019-06-19 20:47:02 +0900

    Adding figures

---
+++ mwarp1d/examples/fig1.py
@@ -0,0 +1,32 @@
+
+import os
+import numpy as np
+from matplotlib import pyplot
+
+
+#load data:
+dir0     = os.path.dirname(__file__)
+fnameCSV = os.path.join(dir0, 'data', 'Dorn2012.csv')
+y        = np.loadtxt(fnameCSV, delimiter=',')
+
+
+#plot:
+pyplot.close('all')
+pyplot.figure( figsize=(6,4) )
+ax = pyplot.axes([0.12,0.12,0.87,0.87])
+h0 = ax.plot(y[0:2].T, color='k')[0]
+h1 = ax.plot(y[2:4].T, color='r')[0]
+h2 = ax.plot(y[4:6].T, color='g')[0]
+h3 = ax.plot(y[6:8].T, color='b')[0]
+ax.legend([h0,h1,h2,h3], ['3.5 m/s', '5.2 m/s', '7.0 m/s', '9.5 m/s'])
+ax.axhline(0, color='k', ls=':')
+ax.text(70, 40, 'Medial', va='center')
+ax.text(70,-40, 'Lateral', va='center')
+ax.set_xlabel('Time  (%)', size=13)
+ax.set_ylabel('Mediolateral ground reaction force  (N)', size=13)
+pyplot.show()
+
+
+# #save figure:
+# fnamePDF = os.path.join(dir0, 'figs', 'fig1.pdf')
+# pyplot.savefig(fnamePDF)
---
+++ mwarp1d/examples/fig2.py
@@ -0,0 +1,55 @@
+
+import os
+import numpy as np
+from matplotlib import pyplot
+import mwarp1d
+
+
+#load data:
+dir0     = os.path.dirname(__file__)
+fnameCSV = os.path.join(dir0, 'data', 'Dorn2012.csv')
+y        = np.loadtxt(fnameCSV, delimiter=',')
+y0,y1    = y[0], y[-1]   #two trials for demonstration
+
+
+#define landmarks and apply warp:
+lm0      = [0, 9, 14, 24, 70, 99]
+lm1      = [0, 11, 22, 33, 73, 99]
+y1w      = mwarp1d.warp1d_landmarks(y1, lm0, lm1)
+
+
+#plot:
+pyplot.close('all')
+pyplot.figure( figsize=(10,4) )
+#create axes:
+ax0 = pyplot.axes([0.08,0.12,0.42,0.84])
+ax1 = pyplot.axes([0.57,0.12,0.42,0.84])
+c0  = (0.3, 0.3, 0.98)
+c1  = (0.98, 0.7, 0.3)
+#plot data and landmarks:
+h0  = ax0.plot(y0, color='0.0', lw=3)[0]
+h1  = ax0.plot(y1, color='0.7', lw=1)[0]
+h2  = ax0.plot(lm0[1:-1], y0[lm0[1:-1]], 'o', color=c0, ms=7)[0]
+h3  = ax0.plot(lm1[1:-1], y1[lm1[1:-1]], 'o', color=c1, ms=7)[0]
+ax0.legend([h0,h1,h2,h3], ['Template', 'Source', 'Template landmarks', 'Source landmarks'], loc='lower right')
+#plot warped data:
+h0  = ax1.plot(y0, color='0.0', lw=3)[0]
+h1  = ax1.plot(y1, color='0.7', lw=1)[0]
+h2  = ax1.plot(y1w, color=c1, lw=2)[0]
+ax1.legend([h0,h1,h2], ['Template', 'Source', 'Warped source'], loc='lower right')
+#annotate:
+for ax in [ax0,ax1]:
+	ax.axhline(0, color='k', ls=':')
+	ax.text(70, 40, 'Medial', va='center')
+	ax.text(70,-40, 'Lateral', va='center')
+	ax.set_xlabel('Time  (%)', size=13)
+ax0.set_ylabel('Mediolateral ground reaction force  (N)', size=13)
+#add panel labels:
+ax0.text(-3, 520, '(a)', size=14)
+ax1.text(-3, 520, '(b)', size=14)
+pyplot.show()
+
+
+# #save figure:
+# fnamePDF = os.path.join(dir0, 'figs', 'fig2.pdf')
+# pyplot.savefig(fnamePDF)
