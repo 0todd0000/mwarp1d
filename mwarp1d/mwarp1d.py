118c3ac040eb1f14b140c46a110701240ac7a8bb
Todd Pataky <todd@h74.50.242.10.103784.vlan.kuins.net>
2019-08-24 08:10:46 +0900

    Clean mwarp1d.py comments

--- mwarp1d/mwarp1d.py
+++ mwarp1d/mwarp1d.py
@@ -598,49 +598,4 @@ def launch_gui(data=None, mode=None, filename_results=None, filename_data=None):
 
 		os.system(command)
 
-	# launch app:
-	
-	
-	
-	
-		
-		
-	
-	
-	#
-	# 
-	# 
-	# print(path2app)
-	#
-	# 
-	
-	
-	# from . main import MainApplication,MainWindow
-	# print(MainApplication)
-	
-	# path2app     = '/Users/todd/Dropbox/2019Sync/Documents/Projects/projects/inlreg1d/Python/ui/main.py'
-	# os.system( path2app )
-	#
-	#
-	# mode         = 'landmarks'
-	# # mode         = 'manual'
-	# # fnameCSV     = '/Users/todd/Dropbox/2019Sync/Documents/Projects/projects/inlreg1d/Data/Case3/sources.csv'
-	# fnameCSV     = '/Users/todd/Dropbox/2019Sync/Documents/Projects/projects/inlreg1d/Data/1000/sources.csv'
-	# fnameNPZ     = '/Users/todd/Desktop/results.npz'
-	# # # fnameNPZ     = dir0 + 'results.npz'
-	# fnameNPZ     = '/Users/todd/Desktop/results.npz'
-	#
-	#
-	# command     = 'python %s %s %s %s' %(path2app, mode, fnameCSV, fnameNPZ)
-	#
-	# os.system( command )
-	
-	
-	
-	# app    = MainApplication(sys.argv)
-	# app.setApplicationName("mwarp1d")
-	# window = MainWindow( sys.argv )
-	# window.move(0, 0)
-	# window.show()
-	# sys.exit(app.exec_())
 
