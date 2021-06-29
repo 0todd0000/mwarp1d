553c8fa99ddd27f9ba01abcf6dc3fd3a866ddcad
Todd Pataky <todd@todds-imac-pro.lan>
2021-06-29 16:28:02 +0900

    Update conda builds

--- mwarp1d/__init__.py
+++ mwarp1d/__init__.py
@@ -3,11 +3,11 @@
 mwarp1d:  Manual one-dimensional data warping and nonlinear registration in Python and PyQt
 
 Copyright (C) 2021  Todd Pataky
-Version: 0.2.1 (2021-06-24)
+Version: 0.2.2 (2021-06-29)
 '''
 
 
-__version__ = '0.2.1'  #2021-06-24
+__version__ = '0.2.2'  #2021-06-29
 
 from . manual import *
 from . landmark import *
